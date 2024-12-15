7744d83e23b23a9bbee3b9ce4cd40a6d840b92e5
Valerii <amabatman.12@gmail.com>
2024-12-15 16:25:37 +0600

    added category and tags moddels

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+class Category(models.Model):
+    title = models.CharField(max_length=56)
+
+    def __str__(self):
+        return self.title
+
+class Tag(models.Model):
+    title = models.CharField(max_length=56)
+
+    def __str__(self):
+        return self.title
+
 class Post(models.Model):
     title = models.CharField(max_length=56)
     content = models.TextField()
@@ -7,6 +19,10 @@ class Post(models.Model):
     created_at = models.DateTimeField(null=True, auto_now_add=True)
     updated_at = models.DateTimeField(null=True, auto_now=True)
     image = models.ImageField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    tags = models.ManyToManyField(Tag, null=True, blank=True)
 
     def __str__(self):
         return f"{self.title}: {self.content}"
+
+
